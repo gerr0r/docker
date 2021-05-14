4d6bc2d4ab0418addcce2f9280c0e3562e00e24f
gerr0r <g.ruschev@gmail.com>
2021-05-14 14:09:13 +0300

    second

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,10 +1,7 @@
 const router = require("express").Router()
 const { Post } = require("../controllers/")
 
-router
-    .route("/")
-    .get(Post.read.all)
-    .post(Post.create)
+router.route("/").get(Post.read.all).post(Post.create)
 router.route("/:id").get(Post.read.one).put(Post.update).delete(Post.delete)
 
 module.exports = router
